730c8e6592847e018b3162fe6261b8d4d5c6226f
yellow-seed <42381113+yellow-seed@users.noreply.github.com>
2025-02-23 14:52:23 +0900

    🔨 Modify vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from 'tailwindcss'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
+  plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [tailwindcss]
+    }
+  }
 })
